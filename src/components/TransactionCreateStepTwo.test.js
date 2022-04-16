353a1c880e551c1c865828953d5ae19252e424e3
Gerry Bong <ggbong734@gmail.com>
2022-04-16 00:07:36 -0700

    halfway through unit test

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", () => {
+    render(<TransactionCreateStepTwo sender={{ id: '5' }} receiver={{ id: '6' }} />);
+    screen.debug();
+});
